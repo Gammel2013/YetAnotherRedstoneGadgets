387c37018b868fab3bfd34550bf158ce52d3dd3c
Gammel2013 <160730477+Gammel2013@users.noreply.github.com>
2024-07-26 08:52:50 +0200

    Rename mod

---
+++ src/main/java/com/gammel2012/yetanotherredstonegadgetsmod/providers/ModItemModelProvider.java
@@ -0,0 +1,34 @@
+package com.gammel2012.yetanotherredstonegadgetsmod.providers;
+
+import com.gammel2012.yetanotherredstonegadgetsmod.registers.ModItems;
+import com.gammel2012.yetanotherredstonegadgetsmod.YetAnotherRedstoneGadgetsMod;
+import com.gammel2012.utils.providers.BaseItemModelProvider;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+
+public class ModItemModelProvider extends BaseItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, YetAnotherRedstoneGadgetsMod.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        cubeAll(
+                getItemName(ModItems.PURPLE_PROPAGATING_REDSTONE_LAMP_ITEM.get()),
+                blockLoc(ModItems.PURPLE_PROPAGATING_REDSTONE_LAMP_ITEM.get()).withSuffix("_off")
+        );
+
+        cubeAll(
+                getItemName(ModItems.BLUE_PROPAGATING_REDSTONE_LAMP_ITEM.get()),
+                blockLoc(ModItems.BLUE_PROPAGATING_REDSTONE_LAMP_ITEM.get()).withSuffix("_off")
+        );
+
+        cubeAll(
+                getItemName(ModItems.RED_PROPAGATING_REDSTONE_LAMP_ITEM.get()),
+                blockLoc(ModItems.RED_PROPAGATING_REDSTONE_LAMP_ITEM.get()).withSuffix("_off")
+        );
+
+        buildBasicItem(ModItems.REDSTONE_DIVIDER_ITEM);
+        buildBasicItem(ModItems.REDSTONE_DIAL_ITEM);
+    }
+}
